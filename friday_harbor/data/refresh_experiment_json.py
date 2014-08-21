2c9d45a5e51b4ae69036e21d893e6b8c9c5d58bc
davidf <davidf@alleninstitute.org>
2014-08-21 10:23:33 -0700

    hack for getting just cre data

--- friday_harbor/data/refresh_experiment_json.py
+++ friday_harbor/data/refresh_experiment_json.py
@@ -22,7 +22,7 @@ import os
 import sys
 from friday_harbor.paths import Paths
 
-def refresh_experiment_json(data_dir='.', injection_structures=None, wild_only=False):
+def refresh_experiment_json(data_dir='.', injection_structures=None, wild=True, cre=True):
 
     # default value for injection structures is gray (structure id 8)
     if injection_structures is None:
@@ -36,10 +36,15 @@ def refresh_experiment_json(data_dir='.', injection_structures=None, wild_only=F
                           '[primary_structure_only$eqtrue]' + \
                           '[injection_structures$eq'+injection_structures+']'
 
-    # if we are restricted to wild type experiments only, add that filter to the query (id for wild = 0)
-    if wild_only:
+    if wild is True and cre is False:
+        # if we are restricted to wild type experiments only, add that filter to the query (id for wild = 0)
         experiment_info_url += '[transgenic_lines$eq0]'
-        
+    elif wild is False and cre is True:
+        # this is the list of all of the transenic lines with projection experiments public as of 8/21/14
+        experiment_info_url += '[transgenic_lines$eq177838542,177837446,177838108,177837315,287226763,177837281,177838225,177839159,177838331,177838138,182693192,182846862,177838907,177838259,177837834,177835893,177839004,177839022,177838899,177837979,177839513,266649644,182761781,177838266,177836019,177838361,177839468,177838877,177838584,177838828,177838302,177839174,177839285,177838502,177838803,177838496,272833052,177839494,177838942,177837788,177839135,177837364,177839044,256980970,177839406,177839331,183238310,177837713,177837275,177839376,177838953,182761918,177838927,177837779,177839425,177838755,183941572,177838022,177839481,177839075,177837262,182901722,177838494,177837858,177838435,179697864,177837695,177837481,177837516,177837637,177837777,177838048,180526080,177837324,177839090,177838074,177838781,177838681,177839294,177837625,177838710,177837328,177839216,177838715,177838730,177837797,177837611,265180449,177838633,177839186,177838622,177838665,177839108,177837585]'
+    elif wild is False and cre is False:
+        return 
+
     # Get data:
     raw_json = requests.request('get', experiment_info_url).json()
     
@@ -53,8 +58,6 @@ if __name__ == "__main__":
     else:
         refresh_experiment_json()
     
-
-
 
 
 
